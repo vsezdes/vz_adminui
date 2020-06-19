8fa232da3f8d33117822b33988434edaf8f8bdf8
Alexei <lexx.kg@gmail.com>
2020-06-19 11:01:20 +0600

    #1 item form improved

--- src/components/ItemForm.vue
+++ src/components/ItemForm.vue
@@ -1,34 +1,100 @@
 <template>
   <v-container>
     <v-card
-      class="mx-auto"
-      max-width="400"
+      class="mx-auto pa-0"
+      max-width="600"
       outlined
+      flat
     >
     <v-card-title class="headline">Добавить товар</v-card-title>
+    <v-stepper v-model="step" vertical non-linear>
+      <v-stepper-step :complete="step > 1" step="1" @click.native="step = 1">
+        Укажите категорию
+        <small>{{ catString }}</small>
+      </v-stepper-step>
+
+      <v-stepper-content step="1">
+        <v-form v-model="valid" class="my-4">
+          <v-select
+            v-model="categoryFirst"
+            label="Категория I уровня"
+            :items="rootCategories"
+            dense
+          />
+          <v-select
+            v-model="categorySecond"
+            :disabled="!categoryFirst"
+            label="Категория II уровня"
+            :items="subRootCategories(categoryFirst)"
+            dense
+          />
+          <v-select
+            v-model="categoryThird"
+            :disabled="!categorySecond"
+            label="Категория III уровня"
+            :items="subRootCategories(categorySecond, true)"
+            dense
+          />
+        </v-form>
+        <v-spacer />
+        <v-btn
+          :disabled="!categoryThird"
+          fab outlined small absolute color="primary"
+          class="next-btn mr-3"
+          v-if="step === 1"
+          @click="step = 2"><v-icon>mdi-arrow-right</v-icon></v-btn>
+      </v-stepper-content>
+
+      <v-stepper-step :complete="step > 2" step="2">
+        Детали товара
+      </v-stepper-step>
+
+      <v-stepper-content step="2">
+        <v-form
+          v-model="detailsValid"
+        >
+          <v-text-field
+            label="Наименование товара"
+            v-model="itemName"
+            :rules="[
+              v => v.length > 5 || 'Обязательно к заполнению (5 букв)'
+            ]"
+          />
+        </v-form>
+        <v-btn
+          :disabled="!detailsValid"
+          fab outlined small absolute color="primary"
+          class="next-btn mr-3"
+          v-if="step === 2"
+          @click="step = 3"><v-icon>mdi-arrow-right</v-icon></v-btn>
+      </v-stepper-content>
+
+      <v-stepper-step :complete="step > 3" step="3">
+        Фото товара
+      </v-stepper-step>
+
+      <v-stepper-content step="3">
+        <v-image-input
+          v-model="imageData"
+          :image-quality="0.85"
+          fullWidth
+          fullHeight
+          clearable
+          image-format="jpeg"
+          flipVerticallyIcon="mdi-format-vertical-align-top"
+          flipHorizontallyIcon="mdi-format-horizontal-align-right"
+        />
+      </v-stepper-content>
+
+      <v-stepper-step step="4">View setup instructions</v-stepper-step>
+      <v-stepper-content step="4">
+        <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
+        <v-btn color="primary" @click="e6 = 1">Continue</v-btn>
+        <v-btn text>Cancel</v-btn>
+      </v-stepper-content>
+    </v-stepper>
     <v-card-actions>
-      <v-form v-model="valid">
-        <v-select
-          v-model="categoryFirst"
-          label="Категория I уровня"
-          :items="rootCategories"
-          dense
-        />
-        <v-select
-          v-model="categorySecond"
-          :disabled="!categoryFirst"
-          label="Категория II уровня"
-          :items="subRootCategories(categoryFirst)"
-          dense
-        />
-        <v-select
-          v-model="categoryThird"
-          :disabled="!categorySecond"
-          label="Категория III уровня"
-          :items="subRootCategories(categorySecond, true)"
-          dense
-        />
-      </v-form>
+
     </v-card-actions>
   </v-card>
 
@@ -37,8 +103,13 @@
 
 <script>
 import CATEGORIES_QUERY from '@/gql/categories.graphql';
+import VImageInput from 'vuetify-image-input';
+
 
 export default {
+  components: {
+    [VImageInput.name]: VImageInput,
+  },
   apollo: {
     categories: CATEGORIES_QUERY,
   },
@@ -48,6 +119,9 @@ export default {
       categorySecond: null,
       categoryThird: null,
       valid: false,
+      step: 3,
+      itemName: '',
+      imageData: '',
     };
   },
   computed: {
@@ -57,14 +131,13 @@ export default {
         value: cat.id,
       }))
     },
+    catString() {
+      return 'suprastin'
+    },
   },
   methods: {
     subRootCategories(catId, nextLevel = false) {
-      const items = [{
-        text: 'Нет',
-        value: 0,
-      }];
-
+      const items = [];
       let theCat = null;
       let children = null;
       if (nextLevel) {
@@ -90,8 +163,15 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .v-card .v-form {
   width: 100%;
+}
+.v-stepper__content {
+  position: relative;
+  .v-btn.next-btn {
+    right: 50px;
+    bottom: -10px;
+  }
 }
 </style>
