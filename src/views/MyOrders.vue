f7cd5d83a534212380c2c31fbcbb7ebf2669f7c0
phoenix-02 <50809139+phoenix-02@users.noreply.github.com>
2020-07-18 10:50:58 +0600

    add table inside table, user & order category filters using select, add user id,code cleanup, add expand table, few trifles fix in Register page

--- src/views/MyOrders.vue
+++ src/views/MyOrders.vue
@@ -1,9 +1,12 @@
 <template>
   <BaseTemplate>
     <v-container fluid>
+      <v-layout wrap justify-center justify-lg-start>
+        <v-flex md2  sm10>
      <v-select
+         @change="CleanupOrders()"
          v-model="selected_user"
-         :items="GetUsers()"
+         :items="all_users"
          item-text="fullName"
          item-value="id"
          label="Select"
@@ -11,63 +14,61 @@
          persistent-hint
          return-object
          single-line
-     ></v-select>
-     <v-select
-         :items="status_titles"
-         item-text="title"
-         item-value="number"
-         v-model="selected_status"
-         :hint="'Выберите категорию заказа'"
-         menu-props="{maxHeight: 404}"
-         persistent-hint
+     >
+     </v-select>
+        </v-flex>
+        <v-flex offset-md-2  md2  sm10>
+         <v-select
+             @change="CleanupOrders()"
+             :items="status_titles"
+             item-text="title"
+             item-value="number"
+             v-model="selected_status"
+             :hint="'Выберите категорию заказа'"
+             menu-props="{maxHeight: 404}"
+             persistent-hint
 
-     ></v-select>
+     >
+
+         </v-select>
+        </v-flex>
+      </v-layout>
     </v-container>
-    <v-row>
-    <v-col cols="1"></v-col>
-    <v-col cols="10">
-    <div v-for="order in fakedata"
-             :key="order.id">
-          <v-card
-              class="mx-auto"
-              max-width="550"
-              v-if="(order.status===selected_status || selected_status==='all' ) && order.user.id === selected_user.id "
-          >
-            <v-card-text>
-              <div>Пользователь: {{ order.user.firstName+' '+order.user.lastName }}</div>
-              <div>Дата заказа: {{ order.orderDate }}</div>
-              <div>Описание: {{ order.details }}</div>
-              <v-menu :offset-y="true" :close-on-content-click="false">
-                <template v-slot:activator="{ on: menu, attrs }">
-                  <v-tooltip bottom>
-                    <template v-slot:activator="{ on: tooltip }">
-                      <v-btn
-                          color="primary"
-                          dark
-                          v-bind="attrs"
-                          v-on="{ ...tooltip, ...menu }"
-                      >Товары</v-btn>
-                    </template>
-                  </v-tooltip>
-                </template>
-                <v-list>
-                  <v-list-item>
-                    <v-data-table
-                        :headers="headers"
-                        :items="order.items"
-                        :items-per-page="5"
+    <v-data-table
+        :headers="order_headers"
+        :items="CleanupOrders()"
+        :items-per-page="999999"
+        item-key="id"
+        hide-default-footer
+        :single-expand="expand"
+        :expanded.sync="expanded"
+        class="elevation-1"
+        show-expand
+        :calculate-widths='true'
+    >
+<!--      Вставка таблицы в таблицу -->
+      <template v-slot:expanded-item="{ headers , item}">
+        <td :colspan="headers.length">
+          <v-data-table
                         hide-default-footer
-                        class="elevation-1"
-                    ></v-data-table>
-                  </v-list-item>
-                </v-list>
-              </v-menu>
-            </v-card-text>
-          </v-card>
-    </div>
-    </v-col>
-    </v-row>
-
+                        class="amber lighten-2"
+                        :items="item.items"
+                        :headers="item_headers"></v-data-table>
+        </td>
+      </template>
+<!--      Определение количества товара -->
+      <template v-slot:item.items="{ item }">
+       {{ item.items.length  }}
+      </template>
+<!--      Определение статуса заказа -->
+      <template v-slot:item.status="{ item }">
+        <v-chip :color="status_titles.find(obj => {return( obj.number === item.status)}).color" >
+            {{ status_titles.find(obj => {
+            return( obj.number === item.status)
+            }).title  }}
+        </v-chip>
+      </template>
+    </v-data-table>
   </BaseTemplate>
 </template>
 
@@ -78,54 +79,67 @@ export default {
   name: "MyOrders",
   data() {
     return {
-      all_users:[],
-      selected_user:null,
-      selected_status:'',
-      status_titles:[
-        {title:'все',number:'all'},
-        {title:'новый',number:1},
-        {title:'готовится',number:2},
-        {title:'доставляется',number:3},
-        {title:'завершен',number:0},
-        {title:'отмена',number:-1},
-        {title:'возврат',number:-2},
+      expanded: [],
+      expand: false,
+      all_users: [],
+      selected_user: null,
+      selected_status: null,
+      status_titles: [
+        {title: 'все', number: 'all',color:null},
+        {title: 'новый', number: 1,color:'blue darken-1'},
+        {title: 'готовится', number: 2,color:'lime darken-2'},
+        {title: 'доставляется', number: 3,color:'amber darken-4'},
+        {title: 'завершен', number: 0,color:'light-green accent-4'},
+        {title: 'отмена', number: -1,color:'red'},
+        {title: 'возврат', number: -2,color:'orange accent-4'},
       ],
-      headers: [
+      order_headers: [
+        {text: 'статус', value: 'status'},
+        {text: 'товары', value: 'items'},
+        {text: 'Пользователь', value: 'user.fullName'},
+        {text: 'описание', value: 'details', width: '23px'},
+        {text: 'сумма', value: 'total'},
+        {text: 'дата', value: 'orderDate'},
+      ],
+      item_headers: [
         {text: 'название товара', value: 'title'},
         {text: 'сумма', value: 'price'},
         {text: 'количество', value: 'quantity'},
       ],
 
       //FAKEDATA
-      fakedata:[{
+      fakedata: [{
         id: 1,
         orderDate: 'some date',
         status: -2, // статусы описаны ниже
-        details: '',
+        details: 'asdasdnfdskjfndjfnsdffs' +
+            'sdfsdfsdfsdf /n' +
+            'sdfsdfsdfsdfs /n' +
+            'sdfsdfsdfsdf /n',
         items: [
-          { title: 'Tovar1', price: 100, quantity: 1 },
-          { title: 'Tovar2', price: 200, quantity: 2 },
-          { title: 'Tovar3', price: 300, quantity: 3 },
+          {title: 'Tovar1', price: 100, quantity: 1},
+          {title: 'Tovar2', price: 200, quantity: 2},
+          {title: 'Tovar3', price: 300, quantity: 3},
         ],
         total: 400,
         user: {
-          id:1,
+          id: 1,
           firstName: 'John',
           lastName: 'Doe',
         }
-      },{
-        id: 1,
+      }, {
+        id: 2,
         orderDate: 'some date',
         status: -1, // статусы описаны ниже
         details: '',
         items: [
-          { title: 'Tovar4', price: 100, quantity: 1 },
-          { title: 'Tovar5', price: 200, quantity: 2 },
-          { title: 'Tovar6', price: 300, quantity: 3 },
+          {title: 'Tovar4', price: 100, quantity: 1},
+          {title: 'Tovar5', price: 200, quantity: 2},
+          {title: 'Tovar6', price: 300, quantity: 3},
         ],
         total: 400,
         user: {
-          id:1,
+          id: 1,
           firstName: 'John',
           lastName: 'Doe',
         }
@@ -135,64 +149,36 @@ export default {
   components: {
     BaseTemplate
   },
-  created() {
-    this.selected_status=this.status_titles[0].number
-    this.users_whitelist=this.GetUsers()
-    console.info(this.users_whitelist)
+  beforeMount() {
+    this.GetUsers()
+    this.selected_status = this.status_titles[0].number
+    this.selected_user=this.all_users[0]
   },
-  methods:{
-    checkit: function () {
-      this.$nextTick(() => {
-        let elt = document.getElementById('john')
-        if(elt.checked) {
-          console.log('checked')
-        } else {
-          console.log('unchecked')
-        }
-      })
-
-      //this.selected.push('Vuejs')
-      //this.selected.push('Paris')
-    },
-    GetUsers(){
-      console.info(this.fakedata)
-      var all_users=[]
+  methods: {
+    GetUsers() {
       let i = 0;
-      for (i=0;i < this.fakedata.length;i++){
-        var order=this.fakedata[i]
-        if (!all_users.find((i) => i === order.user)){
-        order.user.fullName = order.user.firstName + ' ' + order.user.lastName
-        all_users.push(order.user)
+      for (i = 0; i < this.fakedata.length; i++) {
+        var order = this.fakedata[i]
+        if (!this.all_users.includes(order.user)) {
+          order.user.fullName = order.user.firstName + ' ' + order.user.lastName
+          this.all_users.push(order.user)
         }
       }
-
-      console.info(all_users)
-      this.selected_user = all_users[0]
-      return all_users
     },
+    CleanupOrders() {
+      let user = this.selected_user
+      console.log(user)
 
-    // GetItems(status){
-    //   return this.fakedata.items.filter(function(x) { return x.status === status; });
-    // },
-    // eslint-disable-next-line no-unused-vars
-    Check(status=null){
-      // console.log(fakedata[0].items)
-      console.log(this.fakedata[0].items)
-      // var all_items = []
-      // var i
-      // for (i=0;i < this.fakedata.length;i++){
-      //   if (status===this.fakedata[i].status || status === 'all'){
-      //     all_items = all_items.concat(this.fakedata[i].items)
-      //   }
-      // }
-      // console.log(all_items)
-      // return {
-      //   all_items
-      // }
-    }
-  }
+      return this.fakedata.filter((obj) => {
+        if (user != null) {
+          var n = obj.user.id === user.id && obj.status === this.selected_status || this.selected_status === 'all'
+          console.log(n)
+          return n
+        }
+      })
+    },
+  },
 }
-
 </script>
 
 <style scoped>
