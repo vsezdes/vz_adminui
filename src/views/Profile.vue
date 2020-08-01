61bc9b6c8b43bbd734ac4bd7aa3876bcfacd8c71
phoenix-02 <50809139+phoenix-02@users.noreply.github.com>
2020-08-01 20:01:43 +0600

    custom image upload add

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,6 +2,23 @@
   <BaseTemplate>
     <v-layout wrap style="margin-top: 50px" justify-center :disabled="!editable">
       <v-flex offset-0  offset-xs-3 xs6 md2 lg2 class="text-center">
+        <v-btn
+          color="primary"
+          class="text-none"
+          rounded
+          depressed
+          @click="onButtonClick"
+        >
+          Изменить фото профиля
+        </v-btn>
+        <input rules=""
+          ref="uploader"
+          class="d-none"
+          type="file"
+          @change="onAddFiles"
+        />
+        <v-img :src="file.url" ></v-img>
+<!--        <v-btn @click="UploadImage"> asdasdasdasd </v-btn>-->
         <v-avatar rounded size="150px" color="base_header" >
           <v-img v-if="avatar!=='mdi-account'" :src="avatar"/>
           <v-icon color="grey darken-3" v-else size="140px"> {{ user.avatar }}</v-icon>
@@ -24,13 +41,13 @@
               <v-layout wrap justify-space-around>
                 <v-flex xs11 md5 lg5>
                   <v-text-field md5 lg5
-                                :disabled="!editable"
-                                label="* Имя"
-                                name="login"
-                                prepend-icon="mdi-account"
-                                type="text"
-                                v-model="user.firstName"
-                                :rules="nameRules"
+                    :disabled="!editable"
+                    label="* Имя"
+                    name="login"
+                    prepend-icon="mdi-account"
+                    type="text"
+                    v-model="user.firstName"
+                    :rules="nameRules"
                   ></v-text-field>
                 </v-flex>
 
@@ -120,10 +137,12 @@ import BaseTemplate from "@/views/BaseTemplate.vue";
 import { mask } from 'vue-the-mask'
 import gql from "graphql-tag";
 import PasswordChange from "@/components/PasswordChange";
+// import SingleImageUpload from "@/components/SingleImageUpload";
 
 export default {
   name: "Profile",
   components: {
+    // SingleImageUpload,
     PasswordChange,
     BaseTemplate
   },
@@ -133,6 +152,9 @@ export default {
   props:['drawer'],
   data() {
     return {
+      isError: false,
+      errorText: null,
+      file:'',
       user: {...this.$store.state.user},
       id:null,
       editable:false,
@@ -155,21 +177,68 @@ export default {
     }
   },
 methods:{
+  onButtonClick() {
+    this.isSelecting = true
+    window.addEventListener('focus', () => {
+      this.isSelecting = false
+    }, { once: true })
+    this.$refs.uploader.click()
+  },
+  onAddFiles(file) {
+    window.console.log(file.target.files[0]);
+    this.uploadFileToCloudinary(file.target.files[0]).then((fileResponse) => {
+      this.file=fileResponse;
+    });
+  },
+  uploadFileToCloudinary(file) {
+    return new Promise(function (resolve, reject) {
+      //Ideally these to lines would be in a .env file
+      const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/vsetut2020/upload';
+      const CLOUDINARY_UPLOAD_PRESET = 'bc5bf78g';
+      let formData = new FormData();
+      formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+      formData.append('folder', 'cloudinary-demo');
+      formData.append('file', file);
+      let request = new XMLHttpRequest();
+      request.open('POST', CLOUDINARY_URL, true);
+      request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+      request.onreadystatechange = () => {
+        // File uploaded successfully
+        if (request.readyState === 4 && request.status === 200) {
+          let response = JSON.parse(request.responseText);
+          resolve(response);
+        }
+        // Not successfull, let find our what happened
+        if (request.status !== 200) {
+          let response = JSON.parse(request.responseText);
+          let error = response.error.message;
+          this.errorText = 'error uploading files ' + error;
+          this.isError = true;
+          reject(error);
+        }
+      };
+      request.onerror = (err) => {
+        alert('error: ' + err);
+        reject(err);
+      };
+      request.send(formData);
+    });
+  },
     Cancel(){
-      this.user=Object.assign({},this.$store.state.user)
+      this.user={...this.$store.state.user}
     },
     editUser() {
       delete this.user.token, this.user.id, this.user.gender
       this.$apollo.mutate({
         // Query
         mutation : gql`mutation saveUser($id: Int!,$data: UserInput!) {
-            saveUser(id:$id, data:$data,) {
-              phone
-              email
-              firstName
-              lastName
-              token
-            }
+          saveUser(id:$id, data:$data,) {
+            phone
+            email
+            firstName
+            lastName
+            token
+          }
           }`,
         variables: {
           id: 13,
